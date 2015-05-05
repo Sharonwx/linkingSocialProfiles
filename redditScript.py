af030e6a538eaf1b090c113cbaadd7002d3b6894
JLDLaughlin <jlaughli@wellesley.edu>
2015-05-05 10:54:18 -0400

    editing script to include insta

--- redditScript.py
+++ redditScript.py
@@ -3,19 +3,26 @@ import Redditor
 import frequencies
 import createHistograms
 import compare
+import instaInfo
 
 
-
-# the below line will pull usernames off the front page
-getUsernames.getUsernamesFromList('usernames.csv')
+#reddit
+'''
+getUsernames.getUsernamesFromList('usernames.txt')
 getUsernames.doEverything()
-
 userList = getUsernames.getLists()
-
-
 createHistograms.createHistsAndNorms(userList)
+'''
+#instagram, already normalizes
+instaUserList = instaInfo.doEverything()
+print 'user list is',instaUserList
 
-compare.compareAllWordFreqs(userList)
+for user in instaUserList:
+	print instaUserList.getInstagramUser(user)
+
+
+#compare.compareAllWordFreqs(userList,instaUserList)
+
 
 
 

53ee99924d16eea233dd028e50495acc8ed2a511
kshenx3 <kshenx3@users.noreply.github.com>
2015-05-06 01:47:46 -0400

    Update twitter_info.py
    added wordFreq working to get a specific user's word dictionary NOT normalized.
    stuck dealing with TWitter's timestamp..

--- twitter_info.py
+++ twitter_info.py
@@ -1,13 +1,20 @@
 #importing twitter wrapper
 	#json bc that's what twitter returns things usually in
-	#requests to do all the http client-server thingys
-	#collections has a Counter object to tally things
+	#requests to do all the http client-server thingys as needed
+	#collections has a Counter object to tally things, so let's hope it works
 	#prettytable to see if it'll print stuff out formatted nicely [:
 import twitter
 import json
 import requests
-from collections import Counter
-from prettytable import PrettyTable
+#from prettytable import PrettyTable
+
+import twitPerson
+from twitPerson import *
+import createHistograms
+import csv
+
+import string
+import unicodedata
 
 #setting up authorization to query twitter
 CONSUMER_KEY = '2zq826fLWyzsFQFktAl5doane'
@@ -18,12 +25,28 @@ OAUTH_TOKEN_SECRET = 'EmsnQNv1SPN6Yw4BpNGj9Pf3yT68B5s9BRqkpfQdMOaKi'
 auth = twitter.oauth.OAuth(OAUTH_TOKEN, OAUTH_TOKEN_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
 twitter_api = twitter.Twitter(auth=auth)
 
+#initialize a list of twitPersons
+twitList = TwitPpl()
+
 #print twitter_api #this line to just verify that it worked
+
+COUNT = 20 #set
 
 #	findMatches takes in a list input and returns a list of twitter users with matching
 #Reddit usernames
 def findMatches(userlist):
 	twitusers = []
+
+	for user in userlist:
+		try:
+			foundUser = twitter_api.users.lookup(screen_name=user)
+			twitusers.append(user)
+		except:
+			notfoundlist = user + 'was not found.' #+ "\n"
+			print notfoundlist
+
+	print '\nMatched: ' + str(len(twitusers)) + ' out of ' + str(len(userlist)) + ' Reddit usernames\n'
+	return twitusers
 
 	'''
 	What it should do: 
@@ -44,17 +67,6 @@ def findMatches(userlist):
 	print "Matched :" + str(len(twitusers)) + " out of " + str(len(userlist)) + " Reddit usernames"
 	return twitusers 
 	'''
-	for user in userlist:
-		try:
-			foundUser = twitter_api.users.lookup(screen_name=user)
-			twitusers.append(user)
-		except:
-			notfoundlist = user + 'was not found.' #+ "\n"
-			print notfoundlist
-
-	print '\nMatched: ' + str(len(twitusers)) + ' out of ' + str(len(userlist)) + ' Reddit usernames\n'
-	return twitusers
-
 
 
 #	findMatchesFromTxt so that we can just take an input of a txtfile and do same comparisons
@@ -67,31 +79,112 @@ def findMatchesFromTxt(txtfile):
 
 	return findMatches(loadinlist)
 
+#	findMatchesFromCsv to support CSV file parsing of usernames and comparisons
+def findMatchesFromCsv(csvFile):
+	loadinlist = []
+	f = open(csvFile)
+	reader = csv.reader(f)
 
+	for item in reader:
+		for username in item:
+			loadinlist.append(username)
+
+	return findMatches(loadinlist)
+
+
+#	wordFreq takes a user input and gets the 20 most recent tweets (if possible) of the user
+#from their timeline. Then it counts each aggregates all the text and 
 def wordFreq(user):
-	count = 20
+	wordDict = {}
 	tweets = []
-	tweets = twitter_api.statuses.user_timeline(user_id=user, count=count)
 
-	#statuses = []
+	#API call doesn't throw errors if there aren't enough tweets to reach COUNT
+	results = twitter_api.statuses.user_timeline(screen_name=user, count=COUNT)
+	
+	#go through each returned tweet in results and get the unicode text
+	for index, tweet in enumerate(results):
+		tweetText = results[index]["text"]
+		#print tweetText
+		tweets.append(tweetText)
 
-	'''	#print json.dumps(tweets, indent=1)
+	#transform list of unicode tweets into string words and create a dictionary
 	for tweet in tweets:
-		statuses.append(tweet) #[0:19]
-	'''
+		words = tweet.split()
+		table = string.maketrans("","")
 
-	status_texts = [tweets
-		for status in tweets ]
+		#converting from unicode to string, remove punctuation
+		for word in words:
+			stringVer = unicodedata.normalize('NFKD', word).encode('ascii', 'ignore')
+			correctedWord = stringVer.lower().translate(table, string.punctuation)
 
-	words = [ w
-			for t in status_texts
-				for w in t.split()]
+			#add corrected word into dictionary, update count
+			try:
+				wordDict[correctedWord] = wordDict[correctedWord] + 1
+			except:
+				wordDict[correctedWord] = 1 
 
-	for item in words:
-		c = Counter(item)
-		#print c.most_common()
+	#user.setTweetTexts(wordDict) #not used here but should be called later
+	return wordDict
 
-	return c
+#
+def getTweetTimes(user):
+	timeVals = []
+	yearDict = {}
+	monthDict = {}
+	hourDict = {}
+
+	statuses = twitter_api.statuses.user_timeline(screen_name=user, count=20)
+
+	for index, tweet in statuses:
+		timeObj = statuses[index]['created_at']
+		timeVals.append(timeObj)
+
+
+def getTweetWords(user):
+	pass
+
+
+# normAll sets all the normalized dictionaries of a TwitPerson
+def normAll(user):
+	times = countNormTimes(user)
+	
+	user.setNormYears()
+	user.setNormMonths(times[yearDict])
+	user.setNormHours()
+
+	user.setNormWords(normWords(user))
+
+def normWords(user):
+	pass
+
+def normTimes(user):
+	timeVals = []
+	yearDict = {}
+	monthDict = {}
+	hourDict = {}
+
+
+
+
+
+
+#doAllTheThings takes your input file of reddit usernames and finds the matches;
+#creates a TwitPerson and keeps a running list of each TwitPerson created;
+#returns the TwitPeople object, list of TwitPersons
+def doAllTheThings(inputFile):
+	twitusers = findMatchesFromTxt(inputFile)
+	for twit in twitusers:
+		twitPerson = TwitPerson.twitPerson(twit)
+		twitList.addTwitPerson(twitPerson)
+		twitList.addTwitCount()
+	
+	for twit in twitList.getTwitList():
+		normAll(twit)
+
+	return twitList
+
+
+
 
 
 
